2223d7f2bbc3e76aea1509dc7c838fade3e425ec
Bryan Shelton <bryan@sheltonplace.com>
2012-09-17 11:17:56 -0500

    Change some wp-config.php defaults.

--- wp-config.php
+++ wp-config.php
@@ -6,11 +6,8 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	define( 'WP_LOCAL_DEV', true );
 	include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
-	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', '%%DB_NAME%%' );
-	define( 'DB_USER', '%%DB_USER%%' );
-	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
+	print 'provide config';
+  exit;
 }
 
 // ========================
@@ -19,24 +16,31 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
+/** dynamically set siteurl and home options */
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
+define('WP_HOME',    'http://' . $_SERVER['HTTP_HOST']);
+
 // ================================================
 // You almost certainly do not want to change these
 // ================================================
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+// This may be set in local-config.php
+if ( !defined( 'AUTH_KEY' ) ) {
+  // ==============================================================
+  // Salts, for security
+  // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+  // ==============================================================
+  define('AUTH_KEY',         'IT,|Cs6+/vh7zIzEL*Ws2]6 HOycCzL<xE5*ulgMIp@8!F+gs;z>cF]Qfg~a;b5#');
+  define('SECURE_AUTH_KEY',  'I+@I[nkxJY5Tu/MvuxK.+)U/D]C#1O+Wp1/s2_;P ota i1+i8CbFIeh%`7()rz&');
+  define('LOGGED_IN_KEY',    '6;XR,0_XW.g}|*:NM!)~+a(+D|vgT~mGU{drB8oI)QZ$2(+r]8s,7z f-VgO)djy');
+  define('NONCE_KEY',        '%T-y!XbwYKVA<P~|G1f6EM`7pH2kv:vt: `$Yey#]tr8C`c>%/Tdf!%IN2h`a#C>');
+  define('AUTH_SALT',        'kZ*o|Y6?Ha30&y@?5q,HmjbFF&8?Oc+D-hX[y&ij|D-+34T|e.c1$-+,E)gG6aGG');
+  define('SECURE_AUTH_SALT', '{P2%$~fHG6.>>@F^hd2SSABB{I@o1mCSK-E[9F$(p9#u;T-e[<+C`os<uR4aD#7>');
+  define('LOGGED_IN_SALT',   '!C$OB ePwX67;m|;(]7;HFvr0q$+XV-OuU<q4@[zm*7-QY_SA~0girMq4S6CRIg~');
+  define('NONCE_SALT',       '@|h6U=d8`z{C4f:kV]W^N)A|,b-s+FFlV:FIS.De1{4{`Ho=L@fY[+HYG%cr$#*m');
+}
 
 // ==============================================================
 // Table prefix
@@ -68,12 +72,6 @@ define( 'WP_DEBUG_DISPLAY', false );
 // ======================================
 if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
 	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
-
-// ===========================================================================================
-// This can be used to programatically set the stage when deploying (e.g. production, staging)
-// ===========================================================================================
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
 
 // ===================
 // Bootstrap WordPress
