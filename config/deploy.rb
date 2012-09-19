9c20061cecc60d4a07753adf66b0b330ced6955e
Bryan Shelton <bryan@sheltonplace.com>
2012-09-19 09:41:41 -0500

    Do not copy wp/wp-content

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :use_sudo, FALSE
 set :shared_children, %w{config content content}
 set :keep_releases, 3
 default_run_options[:pty] = true
-set :copy_exclude, %w(.git* /Capfile /config/*.rb)
+set :copy_exclude, %w(.git* /Capfile /config/*.rb /wp/wp-content)
 
 set :deploy_to, "/www/apache/html/#{application}"
 server "server.example.com", :web, :app
