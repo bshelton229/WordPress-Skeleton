8d8306347675854a7f2d96bcffbb179c88d1543e
Bryan Shelton <bryan@sheltonplace.com>
2013-03-13 15:36:52 -0500

    Security to remove the generator meta tag.

--- content/mu-plugins/security.php
+++ content/mu-plugins/security.php
@@ -12,3 +12,6 @@ function uwsec_hide_update_notice() {
   endif;
 }
 add_action( 'admin_notices', 'uwsec_hide_update_notice', 1 );
+
+// Remove the wp_generator action
+remove_action('wp_head', 'wp_generator');
