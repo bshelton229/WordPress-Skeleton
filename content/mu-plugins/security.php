8a5055da46c5e8eb55eec0ca560f6350eb23f810
Bryan Shelton <bryan@sheltonplace.com>
2013-03-13 15:23:13 -0500

    Security plugin for standard security implementations.

---
+++ content/mu-plugins/security.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: UWStandardSecurity
+Version: 0.0.1
+Author: Bryan Shelton
+Author URI: https://github.com/bshelton229
+*/
+
+function uwsec_hide_update_notice() {
+  if ( !current_user_can( 'edit_users' ) ) :
+  remove_action( 'admin_notices', 'update_nag', 3 );
+  endif;
+}
+add_action( 'admin_notices', 'uwsec_hide_update_notice', 1 );
